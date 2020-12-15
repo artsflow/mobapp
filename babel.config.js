0e68287029c10d6c22ea34301aaf9cc165a2f095
ciocan <ciocan@gmail.com>
2020-12-15 20:24:16 +0000

    config update

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          components: './src/components',
-          services: './src/services',
-          screens: './src/screens',
-          utils: './src/utils',
-          lib: './src/lib',
-        },
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       root: ['./src'],
+  //       extensions: [
+  //         '.ios.ts',
+  //         '.android.ts',
+  //         '.ts',
+  //         '.ios.tsx',
+  //         '.android.tsx',
+  //         '.tsx',
+  //         '.jsx',
+  //         '.js',
+  //         '.json',
+  //       ],
+  //       alias: {
+  //         components: './src/components',
+  //         services: './src/services',
+  //         screens: './src/screens',
+  //         utils: './src/utils',
+  //         lib: './src/lib',
+  //       },
+  //     },
+  //   ],
+  // ],
 }
