dd2e59e102b32b65f5fdb2ff693f005812f85092
ciocan <ciocan@gmail.com>
2020-12-15 13:47:48 +0000

    remove old nav from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import 'react-native-gesture-handler'
 import * as React from 'react'
 import { ThemeProvider } from 'react-native-magnus'
-import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { SWRConfig } from 'swr'
 
 import { client } from 'services/client'
-import { TabNavigator } from './Tabs'
 
 export const fetcher = (query: string) => client.request(query)
 
@@ -19,11 +17,7 @@ const App = () => {
           fetcher,
         }}
       >
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <TabNavigator />
-          </NavigationContainer>
-        </SafeAreaProvider>
+        <SafeAreaProvider />
       </SWRConfig>
     </ThemeProvider>
   )
