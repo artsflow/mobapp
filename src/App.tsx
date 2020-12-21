0feef357ed76de631cd273a5ae4e4daf6eeb534c
ciocan <ciocan@gmail.com>
2020-12-21 14:58:21 +0000

    add sentry

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import { ThemeProvider } from 'react-native-magnus'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationProvider } from 'react-native-navigation-hooks'
 import { SWRConfig } from 'swr'
+import * as Sentry from '@sentry/react-native'
 
 import { client } from 'services/client'
 
 export const fetcher = (query: string) => client.request(query)
+
+Sentry.init({
+  dsn: 'https://7f596b9fe4374bb4afd7e425b46a9c2e@o494579.ingest.sentry.io/5565940',
+})
 
 const App = (Component: any) => (props: any) => {
   return (
