a670971b0ba3231b59b67c469c893c6aaea41bb2
ciocan <ciocan@gmail.com>
2020-12-29 15:20:54 +0000

    add react-navigation

--- ios/artsflow/AppDelegate.m
+++ ios/artsflow/AppDelegate.m
@@ -2,7 +2,7 @@
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-
+#import <React/RCTRootView.h>
 #import <AppCenterReactNative.h>
 #import <AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNativeCrashes.h>
@@ -37,6 +37,19 @@ static void InitializeFlipper(UIApplication *application) {
   [AppCenterReactNative register];
   [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
   [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge	
+                                                   moduleName:@"artsflow"	
+                                            initialProperties:nil];	
+
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];	
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];	
+  UIViewController *rootViewController = [UIViewController new];	
+  rootViewController.view = rootView;	
+  self.window.rootViewController = rootViewController;	
+  [self.window makeKeyAndVisible];
 
   return YES;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { ThemeProvider } from 'react-native-magnus'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { SWRConfig } from 'swr'
 import * as Sentry from '@sentry/react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { client } from 'services/client'
+import { ActivitiesScreen, MessagesScreen, ProfileScreen, SavedScreen } from 'screens'
 
 export const fetcher = (query: string) => client.request(query)
 
@@ -13,20 +17,36 @@ Sentry.init({
   dsn: 'https://7f596b9fe4374bb4afd7e425b46a9c2e@o494579.ingest.sentry.io/5565940',
 })
 
-const App = (Component: any) => (props: any) => {
+const ActivitiesStack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const ActivitiesStackScreen = () => (
+  <ActivitiesStack.Navigator>
+    <ActivitiesStack.Screen name="Activities" component={ActivitiesScreen} />
+  </ActivitiesStack.Navigator>
+)
+
+const App = () => {
   return (
-    <ThemeProvider>
-      <SWRConfig
-        value={{
-          revalidateOnFocus: false,
-          fetcher,
-        }}
-      >
-        <SafeAreaProvider>
-          <Component {...props} />
-        </SafeAreaProvider>
-      </SWRConfig>
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider>
+        <SWRConfig
+          value={{
+            revalidateOnFocus: false,
+            fetcher,
+          }}
+        >
+          <SafeAreaProvider>
+            <Tab.Navigator>
+              <Tab.Screen name="Activities" component={ActivitiesStackScreen} />
+              <Tab.Screen name="Saved" component={SavedScreen} />
+              <Tab.Screen name="Messages" component={MessagesScreen} />
+              <Tab.Screen name="Profile" component={ProfileScreen} />
+            </Tab.Navigator>
+          </SafeAreaProvider>
+        </SWRConfig>
+      </ThemeProvider>
+    </NavigationContainer>
   )
 }
 
