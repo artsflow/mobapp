97b0e2f5e778239c962bf39c2310e7246422ed92
ciocan <ciocan@gmail.com>
2020-12-30 19:23:01 +0000

    add native stack modal

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,26 +1,21 @@
 import * as React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
+
+import { enableScreens } from 'react-native-screens'
+import { createNativeStackNavigator } from 'react-native-screens/native-stack'
 
 import { TabNavigator } from './Tabs'
 import { ModalScreen } from 'screens'
 
-const RootStack = createStackNavigator()
+enableScreens()
+const RootStack = createNativeStackNavigator()
 
 export function Navigator() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator
-        headerMode="none"
-        screenOptions={{ animationEnabled: false }}
-        mode="modal"
-      >
+      <RootStack.Navigator screenOptions={{ headerShown: false, stackPresentation: 'modal' }}>
         <RootStack.Screen name="Tabs" component={TabNavigator} />
-        <RootStack.Screen
-          name="Modal"
-          component={ModalScreen}
-          options={{ animationEnabled: true }}
-        />
+        <RootStack.Screen name="Modal" component={ModalScreen} />
       </RootStack.Navigator>
     </NavigationContainer>
   )
